3aa805af1f8f5c64a582a0c95a762a355701784a
Victor Barraza <victor.barrazamerino@medieinstitutet.se>
2022-01-03 14:34:50 +0100

    Header
    startpage

---
+++ wp-content/themes/Charizard/functions.php
@@ -0,0 +1,35 @@
+<?php
+
+//Theme assets
+function add_theme_scripts()
+{
+    //header scripts
+    wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css');
+    wp_enqueue_style('style2', get_template_directory_uri() . '/css/style2.css');
+    
+}
+add_action('wp_enqueue_scripts', 'add_theme_scripts');
+
+//visar vilken sida i template hierarkien som man är i(när man är inloggad)
+function show_template_page()
+{
+    if (is_super_admin()) {
+        global $template;
+        print_r($template);
+    }
+}
+add_action('wp_footer', 'show_template_page');
+
+//Theme support
+add_theme_support('menus');
+add_theme_support('widgets');
+add_theme_support('post-thumbnails');
+
+//Registrerar en header meny
+function register_menu()
+{
+    register_nav_menu('header-menu', 'Header Menu');
+}
+add_action('after_setup_theme', 'register_menu');
+
+?>
