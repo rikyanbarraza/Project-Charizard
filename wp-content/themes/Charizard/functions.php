de5a8041845dc47276b4d4c7e9e0101d0898835d
Victor Barraza <victor.barrazamerino@medieinstitutet.se>
2022-01-07 12:24:24 +0100

    ändringar
    test

--- wp-content/themes/Charizard/functions.php
+++ wp-content/themes/Charizard/functions.php
@@ -6,6 +6,7 @@ function add_theme_scripts()
     //header scripts
     wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css');
     wp_enqueue_style('style2', get_template_directory_uri() . '/css/style2.css');
+    wp_enqueue_style('style3', get_template_directory_uri() . '/css/style3.css');
     
 }
 add_action('wp_enqueue_scripts', 'add_theme_scripts');
@@ -24,12 +25,14 @@ add_action('wp_footer', 'show_template_page');
 add_theme_support('menus');
 add_theme_support('widgets');
 add_theme_support('post-thumbnails');
+add_theme_support( 'woocommerce' );
+
 
 //Registrerar en header meny
 function register_menu()
 {
     register_nav_menu('header-menu', 'Header Menu');
 }
-add_action('after_setup_theme', 'register_menu');
+add_action('after_setup_theme', 'register_menu', 'customtheme_add_woocommerce_support');
 
 ?>
