c5254982aa59ca78b481ac46191b5acd121d5624
Victor Barraza <victor.barrazamerino@medieinstitutet.se>
2022-01-13 14:49:42 +0100

    delete

--- wp-content/plugins/fraktjakt-shipping-for-woocommerce/js/scripts.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function(){
-  jQuery('a.fraktjakt-view-shipment, a.fraktjakt-track-shipment, a.fraktjakt-handle-order, a.fraktjakt-handle-shipment').attr('target', '_blank');
-});
--- wp-content/plugins/google-listings-and-ads/js/build/custom-inputs.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'c7e88228723cbb38ea83db731e8983ed');
