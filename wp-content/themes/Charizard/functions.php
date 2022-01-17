f57511a83a5887bc6eb1f4e5920dc92b241cca71
Victor Barraza <victor.barrazamerino@medieinstitutet.se>
2022-01-17 15:59:20 +0100

    Fixat varukorgen

--- wp-content/themes/Charizard/functions.php
+++ wp-content/themes/Charizard/functions.php
@@ -7,6 +7,7 @@ function add_theme_scripts()
     wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css');
     wp_enqueue_style('style2', get_template_directory_uri() . '/css/style2.css');
     wp_enqueue_style('style3', get_template_directory_uri() . '/css/style3.css');
+    wp_enqueue_style('style4', get_template_directory_uri() . '/css/style4.css');
     
 }
 add_action('wp_enqueue_scripts', 'add_theme_scripts');
@@ -35,4 +36,61 @@ function register_menu()
 }
 add_action('after_setup_theme', 'register_menu', 'customtheme_add_woocommerce_support');
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
+// ----------------------------------------------------------------------------------------------------
+
+
+//ändra i varukorg
+add_filter( 'gettext', 'woocommerce_rename_coupon_field_on_cart', 10, 3 );
+add_filter( 'gettext', 'woocommerce_rename_coupon_field_on_cart', 10, 3 );
+add_filter('woocommerce_coupon_error', 'rename_coupon_label', 10, 3);
+add_filter('woocommerce_coupon_message', 'rename_coupon_label', 10, 3);
+add_filter('woocommerce_cart_totals_coupon_label', 'rename_coupon_label',10, 1);
+add_filter( 'woocommerce_checkout_coupon_message', 'woocommerce_rename_coupon_message_on_checkout' );
+
+
+function woocommerce_rename_coupon_field_on_cart( $translated_text, $text, $text_domain ) {
+	// bail if not modifying frontend woocommerce text
+	if ( is_admin() || 'woocommerce' !== $text_domain ) {
+		return $translated_text;
+	}
+	if ( 'Coupon:' === $text ) {
+		$translated_text = 'Rabattkod:';
+	}
+
+	if ('Coupon has been removed.' === $text){
+		$translated_text = 'Rabattkod har blivit bortagen.';
+	}
+
+	if ( 'Apply coupon' === $text ) {
+		$translated_text = 'Aktivera Rabattkod';
+	}
+
+	if ( 'Coupon code' === $text ) {
+		$translated_text = 'Rabattkod';
+	
+	} 
+    if ( 'Update cart' === $text){
+		$translated_text = 'Uppdatera';
+    }
+
+	return $translated_text;
+}
+
+
+// rename the "Have a Coupon?" message on the checkout page
+function woocommerce_rename_coupon_message_on_checkout() {
+	return 'Have an Offer Code?' . ' ' . __( 'Click here to enter your code', 'woocommerce' ) . '';
+}
+
+
+function rename_coupon_label($err, $err_code=null, $something=null){
+
+	$err = str_ireplace("Coupon","Offer Code ",$err);
+
+	return $err;
+}
+
+
+// Tabort meddelande
+
 ?>
