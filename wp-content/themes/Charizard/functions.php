eea990140edc439035d7a1bd30fd3821c1e1533a
Victor Barraza <victor.barrazamerino@medieinstitutet.se>
2022-01-21 08:44:00 +0100

    ändringar CSS

--- wp-content/themes/Charizard/functions.php
+++ wp-content/themes/Charizard/functions.php
@@ -91,4 +91,33 @@ function rename_coupon_label($err, $err_code=null, $something=null){
 	return $err;
 }
 
+/**
+ * WooCommerce Show Product Image in Checkout Page
+*/
+
+add_filter( 'woocommerce_cart_item_name', 'ts_product_image_on_checkout', 10, 3 );
+
+function ts_product_image_on_checkout( $name, $cart_item, $cart_item_key ) {  
+
+    /* Return if not checkout page */
+    if ( ! is_checkout() ) {
+        return $name;
+    }
+
+    /* Get product object */
+    $_product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+
+    /* Get product thumbnail */
+    $thumbnail = $_product->get_image();
+
+    /* Add wrapper to image and add some css */
+    $image = '<div class="ts-product-image">'
+                . $thumbnail .
+            '</div>';
+
+    /* Prepend image to name and return it */
+    return $image . $name;
+
+}
+
 ?>
