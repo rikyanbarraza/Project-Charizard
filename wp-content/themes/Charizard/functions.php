5cfda1018984f08e02bdf8118873903eb7310d34
Victor Barraza <victor.barrazamerino@medieinstitutet.se>
2022-01-31 14:31:33 +0100

    admin
    admin

--- wp-content/themes/Charizard/functions.php
+++ wp-content/themes/Charizard/functions.php
@@ -10,6 +10,7 @@ function add_theme_scripts()
     wp_enqueue_style('style3', get_template_directory_uri() . '/css/style3.css');
     wp_enqueue_style('style4', get_template_directory_uri() . '/css/style4.css');
 	wp_enqueue_style('style5', get_template_directory_uri() . '/css/style5.css');
+    wp_enqueue_style('style6', get_template_directory_uri() . '/css/style6.css');
     
 }
 add_action('wp_enqueue_scripts', 'add_theme_scripts');
@@ -135,7 +136,10 @@ function header_add_to_cart_fragment( $fragments ) {
     ob_start();
 
     ?>
-    <a class="cart-customlocation" href="<?php echo esc_url(wc_get_cart_url()); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>"><?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count);?> - <?php echo $woocommerce->cart->get_cart_total(); ?></a>
+    <a class="cart-customlocation" href="<?php echo esc_url(wc_get_cart_url()); ?>" 
+    title="<?php _e('View your shopping cart', 'woothemes'); ?>">
+    <?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count);?>
+     - <?php echo $woocommerce->cart->get_cart_total(); ?></a>
     <?php
     $fragments['a.cart-customlocation'] = ob_get_clean();
 
