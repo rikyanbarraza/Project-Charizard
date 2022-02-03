66cc8278114dc0096443c91d59d53faee8241040
Victor Barraza <victor.barrazamerino@medieinstitutet.se>
2022-02-03 14:46:30 +0100

    Update functions.php

--- wp-content/themes/Charizard/functions.php
+++ wp-content/themes/Charizard/functions.php
@@ -176,3 +176,22 @@ add_filter( 'woocommerce_shipping_package_name', 'custom_shipping_package_name' 
 function custom_shipping_package_name( $name ) {
     return 'Frakt';
 }
+// To change add to cart text on single product page
+
+add_filter( 'woocommerce_product_single_add_to_cart_text', 'woocommerce_custom_single_add_to_cart_text' );
+
+function woocommerce_custom_single_add_to_cart_text() {
+
+    return __( 'Köp nu', 'woocommerce' );
+
+}
+
+// To change add to cart text on product archives(Collection) page
+
+add_filter( 'woocommerce_product_add_to_cart_text', 'woocommerce_custom_product_add_to_cart_text' ); 
+
+function woocommerce_custom_product_add_to_cart_text() {
+
+    return __( 'Köp nu', 'woocommerce' );
+
+}
